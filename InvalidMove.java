c90b94cd891fa1009d19149fe316ea82a7ab7cc7
Hector Meley <hector.meley@eleves.ec-nantes.fr>
2017-02-14 15:17:19 +0100

    Add files via upload
    Voilà le code initial avec quelque fonctions de test, il manque la javadoc mais sinon tout fonctionne à priori.

---
+++ InvalidMove.java
@@ -0,0 +1,17 @@
+package othellopgroup;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hector
+ */
+class InvalidMove extends Exception {
+    public void InvalidMove(){
+        System.out.println("Invalid Move");
+    }
+}
